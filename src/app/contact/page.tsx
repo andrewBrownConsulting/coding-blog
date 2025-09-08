217364d085a824ecc0fc8f17afb7f46b069c68a8
Andrew Brown <andrewbrownconsult@gmail.com>
2025-09-08 10:37:47 +0100

    update components dir

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
             <p>Email: <a href="mailto:andrew.brown75@yahoo.com">andrew.brown75@yahoo.com</a></p>
             <p>Message me on <a href='https://www.linkedin.com/in/andrew-brown-b50592183/'>Linkedin</a></p>
             <p>Look at my <a href='https://www.github.com/andrewBrownConsulting/'>Github</a></p>
-            <p>Email: andrew.brown75@yahoo.com</p>
+            <p>Available on Upwork at <a href="https://upwork.com/freelancers/andrewb93">Upwork</a></p>
         </div>
     )
 }
