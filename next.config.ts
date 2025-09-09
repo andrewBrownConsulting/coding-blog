b55a2d410f484d580cdf707cd02be048e6485d08
Andrew Brown <andrewbrownconsult@gmail.com>
2025-09-09 19:18:51 +0100

    update next config for imgur

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'i.imgur.com' }]
-  }
+    domains: ['i.imgur.com']
+  },
+  remotePatterns: [
+    { protocol: 'https', hostname: 'i.imgur.com' }]
 };
 
 export default nextConfig;
