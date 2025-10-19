e5032f36eb1e976ecf1b6eef298f40e4f309cee9
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2025-10-19 08:19:06 +0100

    fix bug with double question mark

--- frontend/src/app/utils.js
+++ frontend/src/app/utils.js
@@ -1,11 +1,13 @@
 export function parseDate(dateString) {
     return new Date(Date.parse(dateString)).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" });
 }
-const imagesURL = process.env.NEXT_PUBLIC_IMAGES_BASE_URL?? process.env.NEXT_PUBLIC_IMAGES_BASE_URL: 'https://blog-images-s3-andrewb.s3.us-east-1.amazonaws.com/'
+const imagesURL = process.env.NEXT_PUBLIC_IMAGES_BASE_URL? process.env.NEXT_PUBLIC_IMAGES_BASE_URL: 'https://blog-images-s3-andrewb.s3.us-east-1.amazonaws.com/'
 export function getImageFromId(id, card = false) {
-    return (<img
-        className={'img-fluid rounded' + (card ? ' card-img' : '')}
+    return (
+    <img
+        className={'img-fluid rounded ' + (card ? 'card-img' : '')}
         src={imagesURL + id + '.png'}
         alt='Post'
-    />)
+    />
+  )
 }
