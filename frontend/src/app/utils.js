281e4d08b47a3fd153841784a1dac6e88fa4058d
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2025-10-19 08:15:27 +0100

    fix build issues with .env

--- frontend/src/app/utils.js
+++ frontend/src/app/utils.js
@@ -1,10 +1,11 @@
 export function parseDate(dateString) {
     return new Date(Date.parse(dateString)).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" });
 }
+const imagesURL = process.env.NEXT_PUBLIC_IMAGES_BASE_URL?? process.env.NEXT_PUBLIC_IMAGES_BASE_URL: 'https://blog-images-s3-andrewb.s3.us-east-1.amazonaws.com/'
 export function getImageFromId(id, card = false) {
     return (<img
         className={'img-fluid rounded' + (card ? ' card-img' : '')}
-        src={process.env.NEXT_PUBLIC_IMAGES_BASE_URL + id + '.png'}
+        src={imagesURL + id + '.png'}
         alt='Post'
     />)
 }
